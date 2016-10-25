e615b03fc680320da52a11cf6344553e8a7062d4
Eileen Kuehn <eileen.kuehn@kit.edu>
2016-10-25 16:59:09 +0200

    adapted some docstrings to use wording from DenGraph algorithm

--- dengraph/dengraph.py
+++ dengraph/dengraph.py
@@ -7,10 +7,16 @@ class DenGraphIO(dengraph.graph.Graph):
     """
     Density Graph allowing for Overlap and Incremental updates.
     """
-    def __init__(self, base_graph, cluster_distance, center_neighbours):
+    def __init__(self, base_graph, cluster_distance, core_neighbours):
+        """
+
+        :param base_graph: the underlying graph
+        :param cluster_distance: eta
+        :param core_neighbours: epsilon
+        """
         self.graph = base_graph
         self.cluster_distance = cluster_distance
-        self.center_neighbours = center_neighbours
+        self.core_neighbours = core_neighbours
         self.clusters = []
         self._init_cluster()
 
@@ -18,9 +24,9 @@ class DenGraphIO(dengraph.graph.Graph):
         """Perform initial cluster"""
         self.clusters = type(self.clusters)()
         # Avoid nodes for which a decision has been made:
-        # - Centers can only belong to one cluster; once a node is a cluster
-        #   center, it cannot change state.
-        # - Junk can NEVER be close to center or fringe, so it will never
+        # - Core nodes can only belong to one cluster; once a node is a cluster
+        #   core node, it cannot change state.
+        # - Junk can NEVER be close to core or border nodes, so it will never
         #   show up again as neighbour.
         # - Border nodes may belong to multiple clusters, we WANT to inspect
         #   them again for each cluster.
@@ -30,24 +36,24 @@ class DenGraphIO(dengraph.graph.Graph):
                 continue
             neighbours = self.graph.get_neighbours(node)
             # We haven't seen it, so it cannot be touched by any existing
-            # cluster. If it's a center, it MUST form a new cluster.
-            # If it's not a center, either a center will claim it or it's
+            # cluster. If it's a core node, it MUST form a new cluster.
+            # If it's not a core node, either a core will claim it or it's
             # junk. We just leave it to be claimed or remain junk.
-            if len(neighbours) >= self.center_neighbours:
+            if len(neighbours) >= self.core_neighbours:
                 finalized.add(node)
                 this_cluster = dengraph.cluster.DenGraphCluster(self.graph)
                 this_cluster.categorize_node(node, this_cluster.CORE_NODE)
                 # stack of neighbours we must inspect
                 # any neighbour belongs to the cluster, by definition.
                 unchecked = collections.deque(neighbours)
-                # A center's neighbour is automatically part of the cluster.
+                # A core's neighbour is automatically part of the cluster.
                 # We recurse neighbour-to-neighbour to find the entire cluster.
                 while unchecked:  # nodes from recursive iteration over cluster
                     neighbour = unchecked.popleft()
                     neighbours = self.graph.get_neighbours(neighbour)
                     # This neighbour is another center which extends the
-                    # cluster, so all its neighbours are at least fringe nodes.
-                    if len(neighbours) >= self.center_neighbours:
+                    # cluster, so all its neighbours are at least border nodes.
+                    if len(neighbours) >= self.core_neighbours:
                         finalized.add(node)
                         this_cluster.categorize_node(node, this_cluster.CORE_NODE)
                         unchecked.extend(nnode for nnode in neighbours if nnode not in finalized)
