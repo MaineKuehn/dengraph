1c285a82696b1fd2df918f0584fd5d166bbd5a60
Max Fischer <maxfischer2781@gmail.com>
2016-10-25 13:47:10 +0200

    DistanceGraph now implements Graph interface

--- dengraph/graphs/distance_graph.py
+++ dengraph/graphs/distance_graph.py
@@ -3,7 +3,7 @@ from dengraph import graph
 
 class DistanceGraph(graph.Graph):
     """
-    Graph
+    Graph of nodes connected by a distance function
     """
     def __init__(self, nodes, distance, symmetric=True):
         self._nodes = set(nodes)
@@ -17,30 +17,56 @@ class DistanceGraph(graph.Graph):
     def __len__(self):
         return len(self._nodes)
 
-    def __getitem__(self, row_col):
-        row, col = row_col
-        if col > row and self.symmetric:
-            col, row = row, col
-        try:
-            return self._distance_values[row, col]
-        except KeyError:
-            self._compute_element(row, col)
-            return self._distance_values[row, col]
+    def __getitem__(self, item):
+        # a:b -> slice -> edge
+        if isinstance(item, slice):
+            assert item.step is None, '%s does not support stride argument for edges' % self.__class__.__name__
+            node_from, node_to = item.start, item.stop
+            # Since we don't know the type of nodes, we cannot test
+            # node_to > node_from to detect swapped pairs. Since we
+            # *do* store nodes in a `set`, they must support hash.
+            if self.symmetric and hash(node_to) > hash(node_from):
+                node_to, node_from = node_from, node_to
+            try:
+                return self._distance_values[node_from, node_to]
+            except KeyError:
+                self._compute_distance(node_from, node_to)
+                return self._distance_values[node_from, node_to]
+        else:
+            raise TypeError('Not an edge: %s' % item)
 
-    def _compute_element(self, row, col, force=False):
-        if not force and (row, col) in self._distance_values:
+    def __setitem__(self, key, value):
+        raise TypeError('%s does not support edge assignment' % self.__class__.__name__)
+
+    def __delitem__(self, item):
+        # a:b -> slice -> edge
+        if isinstance(item, slice):
+            raise TypeError('%s does not support edge deletion' % self.__class__.__name__)
+        else:
+            try:
+                self._nodes.remove(item)
+            except KeyError:
+                raise graph.NoSuchNode
+            else:
+                # clean up all stored distances
+                for node in self:
+                    self._distance_values.pop(item, node)
+                    if self.symmetric:
+                        continue
+                    self._distance_values.pop(node, item)
+
+    def _compute_distance(self, node_from, node_to, force=False):
+        if not force and (node_from, node_to) in self._distance_values:
             return
-        self._distance_values[row, col] = self.distance(self._nodes[row], self._nodes[col])
-        if self._distance_values[row, col] > self._max:
-            self._max = self._distance_values[row, col]
-        if self._distance_values[row, col] < self._min:
-            self._min = self._distance_values[row, col]
+        self._distance_values[node_from, node_to] = self.distance(node_from, node_to)
 
     def __iter__(self):
         return iter(self._nodes)
 
     def get_neighbours(self, node, distance):
-        raise NotImplementedError
+        neighbours = [candidate for candidate in self if self[node, candidate] <= distance]
+        neighbours.sort()
+        return neighbours
 
     def insert_node(self, node):
         self._nodes.add(node)
