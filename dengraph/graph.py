804afc58d1e3e7cfc8b941bde1e6ce71b0f7540a
Max Fischer <maxfischer2781@gmail.com>
2016-10-26 13:14:05 +0200

    expanded definition for setting nodes

--- dengraph/graph.py
+++ dengraph/graph.py
@@ -45,6 +45,17 @@ class Graph(dengraph.compat.ABCBase):
       Remove the edge between nodes `a` and `b` from `g`.  Raises
       :exc:`NoSuchEdge` if the edge is not in the graph.
 
+    .. describe:: g[a] = {}
+                  g[a] = None
+
+      Add the node `a` to graph `g`, without explicit edges. Graphs for which
+      edges are computed, not set, may create them automatically.
+
+    .. describe:: g[a] = {b: ab_edge, c: ac_edge, ...}
+
+      Add the node `a` to graph `g` if it is not present. Set the edge between
+      nodes `a` and `b` to `ab_edge`, between `a` and `c` to `ac_edge`, etc.
+
     .. describe:: del g[a]
 
       Remove the node `a` and all its edges from `g`.  Raises
@@ -66,12 +77,24 @@ class Graph(dengraph.compat.ABCBase):
         raise NotImplementedError
 
     def __getitem__(self, item):
+        # gr[node_a:node_b]
+        # => gr.__getitem__(item=slice(a, b))
+        # gr[node_a]
+        # => gr.__getitem__(item=node_a)
         raise NotImplementedError
 
     def __setitem__(self, item, value):
+        # gr[node_a:node_b] = distance
+        # => gr.__setitem__(item=slice(a, b), value=distance)
+        # gr[node_a] = {node_b: dist_b, node_c: dist_c}
+        # => gr.__setitem__(item=node_a, value={node_b: dist_b, node_c: dist_c})
         raise NotImplementedError
 
     def __delitem__(self, item):
+        # del gr[node_a:node_b]
+        # => gr.__delitem__(item=slice(a, b))
+        # del gr[node_a]
+        # => gr.__delitem__(item=node_a)
         raise NotImplementedError
 
     def __iter__(self):
