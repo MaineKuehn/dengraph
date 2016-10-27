162def6084d0ccfbc66661e9b41aacda420a3a2a
Max Fischer <maxfischer2781@gmail.com>
2016-10-27 11:13:09 +0200

    added container pretty-printer

---
+++ dengraph/utilities/pretty.py
@@ -0,0 +1,39 @@
+from __future__ import absolute_import, division
+import dengraph.compat
+
+
+def repr_container(container, max_elements=1, max_chars=10):
+    """
+    Get a shortened representation of a container, showing only limited elements
+
+    :param container: an iterable container
+    :param max_elements: maximum number of elements to display
+    :type max_elements: int
+    :param max_chars: maximum number of characters to show if container is a string
+    :type max_chars: int or None
+
+    If `max_chars is None`, it defaults to `max_elements`.
+    """
+    max_chars = max_elements if max_chars is None else max_chars
+    if len(container) <= max_elements:
+        return repr(container)
+    if isinstance(container, str):
+        if len(container) <= max_chars:
+            return repr(container)
+        return '"%s...[+%d]"' % (container[:max_chars], len(container) - max_chars)
+    elif isinstance(container, (dict, dengraph.compat.collections_abc.Mapping)):
+        citer = iter(container)
+        item_repr = ', '.join('%r: %r' % (key, container[key]) for key in (next(citer) for _ in range(max_elements)))
+    else:  # elif isinstance(container, (set, frozenset, list, tuple)):
+        citer = iter(container)
+        item_repr = ', '.join(repr(next(citer)) for _ in range(max_elements))
+    start, stop = container_symbols.get(type(container), '<>')
+    return start + item_repr + ', <%d elements>' % (len(container) - max_elements) + stop
+
+container_symbols = {
+    list: '[]',
+    tuple: '()',
+    dict: '{}',
+    set: '{}',
+    frozenset: ('f{', '}'),
+}
