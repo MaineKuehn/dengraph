1b511e78ab1aea3bc8dd576b199746fed20f779f
Max Fischer <maxfischer2781@gmail.com>
2016-10-30 22:04:24 +0100

    added tests for graph_io headers

--- dengraph_unittests/graphs_unittests/test_graph_io.py
+++ dengraph_unittests/graphs_unittests/test_graph_io.py
@@ -7,6 +7,14 @@ import dengraph.graphs.graph_io
 
 
 class GraphIOTest(unittest.TestCase):
+    @staticmethod
+    def generate_matrix_csv(size=4, separator=','):
+        literal = '\n'.join(
+            separator.join(str(rval - cval) for cval in range(size))
+            for rval in range(size)
+        )
+        return literal
+
     def test_default(self):
         """
         Test evaluation using default settings
@@ -47,3 +55,43 @@ class GraphIOTest(unittest.TestCase):
         with self.assertRaises(dengraph.graph.NoSuchEdge):
             graph['d':'c']
 
+    def test_header_none(self):
+        """Test default, enumerated header"""
+        for size in (1, 5, 10, 20):
+            literal = self.generate_matrix_csv(size)
+            graph = dengraph.graphs.graph_io.csv_graph_reader(literal.splitlines(), nodes_header=False)
+            self.assertHeaderMatrixGraph(list(range(size)), graph)
+
+    def test_header_iterable(self):
+        """Test header from iterable"""
+        for size in (1, 5, 10, 20):
+            header = ['N%02d' % num for num in range(size)]
+            literal = self.generate_matrix_csv(size)
+            graph = dengraph.graphs.graph_io.csv_graph_reader(literal.splitlines(), nodes_header=header)
+            self.assertHeaderMatrixGraph(header, graph)
+
+    def test_header_strings(self):
+        """Test header from first line as strings"""
+        for size in (1, 5, 10, 20):
+            header = ['N%02d' % num for num in range(size)]
+            literal = ','.join(header) + '\n' + self.generate_matrix_csv(size)
+            graph = dengraph.graphs.graph_io.csv_graph_reader(literal.splitlines(), nodes_header=True)
+            self.assertHeaderMatrixGraph(header, graph)
+
+    def test_header_call(self):
+        """Test header from first line as strings"""
+        for size in (1, 5, 10, 20):
+            header = ['%2d' % num for num in range(size)]
+            literal = ','.join(header) + '\n' + self.generate_matrix_csv(size)
+            graph = dengraph.graphs.graph_io.csv_graph_reader(literal.splitlines(), nodes_header=lambda elem: int(elem))
+            self.assertHeaderMatrixGraph(list(range(size)), graph)
+
+    def assertHeaderMatrixGraph(self, header, graph):
+            self.assertEqual(sorted(graph), sorted(header))
+            for row_idx, node_from in enumerate(header):
+                for column_idx, node_to in enumerate(header):
+                    if column_idx == row_idx:
+                        with self.assertRaises(dengraph.graph.NoSuchEdge):
+                            graph[node_from:node_to]
+                    else:
+                        self.assertEqual(graph[node_from:node_to], row_idx - column_idx)
