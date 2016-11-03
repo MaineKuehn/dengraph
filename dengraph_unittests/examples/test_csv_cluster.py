3ca2f2b3ea11a34e563818ad26d78c157634522c
Max Fischer <maxfischer2781@gmail.com>
2016-11-03 16:47:51 +0100

    added examples

---
+++ dengraph_unittests/examples/test_csv_cluster.py
@@ -0,0 +1,7 @@
+from dengraph.graphs.graph_io import csv_graph_reader
+from dengraph.dengraph import DenGraphIO
+
+# distance matrix defined as CSV
+data = """
+Alfons Bernard Charlie Dirk Eduard
+"""
