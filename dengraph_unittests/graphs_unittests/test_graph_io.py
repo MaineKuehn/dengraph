0972aafed429f09203e7af25aefe3b1fc7a55eb3
Max Fischer <maxfischer2781@gmail.com>
2016-11-05 18:12:20 +0100

    testing csv reader for symmetric and asymmetric input

--- dengraph_unittests/graphs_unittests/test_graph_io.py
+++ dengraph_unittests/graphs_unittests/test_graph_io.py
@@ -1,16 +1,20 @@
 import textwrap
-import unittest
 
 
 import dengraph.graph
 import dengraph.graphs.graph_io
 
+from dengraph_unittests.utility import unittest
+
 
 class GraphIOTest(unittest.TestCase):
     @staticmethod
-    def generate_matrix_csv(size=4, separator=','):
+    def generate_matrix_csv(size=4, separator=',', symmetric=False):
         literal = '\n'.join(
-            separator.join(str(rval - cval) for cval in range(size))
+            separator.join(
+                str(abs(rval - cval) if symmetric is False else (cval - rval))
+                for cval in range(size)
+            )
             for rval in range(size)
         )
         return literal
@@ -57,34 +61,50 @@ class GraphIOTest(unittest.TestCase):
 
     def test_header_none(self):
         """Test default, enumerated header"""
-        for size in (1, 5, 10, 20):
-            literal = self.generate_matrix_csv(size)
-            graph = dengraph.graphs.graph_io.csv_graph_reader(literal.splitlines(), nodes_header=False)
-            self.assertHeaderMatrixGraph(list(range(size)), graph)
+        for symmetric in (True, False):
+            with self.subTest(symmetric=symmetric):
+                for size in (1, 5, 10, 20):
+                    literal = self.generate_matrix_csv(size, symmetric=symmetric)
+                    graph = dengraph.graphs.graph_io.csv_graph_reader(
+                        literal.splitlines(), nodes_header=False, symmetric=symmetric
+                    )
+                    self.assertHeaderMatrixGraph(list(range(size)), graph)
 
     def test_header_iterable(self):
         """Test header from iterable"""
-        for size in (1, 5, 10, 20):
-            header = ['N%02d' % num for num in range(size)]
-            literal = self.generate_matrix_csv(size)
-            graph = dengraph.graphs.graph_io.csv_graph_reader(literal.splitlines(), nodes_header=header)
-            self.assertHeaderMatrixGraph(header, graph)
+        for symmetric in (True, False):
+            with self.subTest(symmetric=symmetric):
+                for size in (1, 5, 10, 20):
+                    header = ['N%02d' % num for num in range(size)]
+                    literal = self.generate_matrix_csv(size, symmetric=symmetric)
+                    graph = dengraph.graphs.graph_io.csv_graph_reader(
+                        literal.splitlines(), nodes_header=header, symmetric=symmetric
+                    )
+                    self.assertHeaderMatrixGraph(header, graph)
 
     def test_header_strings(self):
         """Test header from first line as strings"""
-        for size in (1, 5, 10, 20):
-            header = ['N%02d' % num for num in range(size)]
-            literal = ','.join(header) + '\n' + self.generate_matrix_csv(size)
-            graph = dengraph.graphs.graph_io.csv_graph_reader(literal.splitlines(), nodes_header=True)
-            self.assertHeaderMatrixGraph(header, graph)
+        for symmetric in (True, False):
+            with self.subTest(symmetric=symmetric):
+                for size in (1, 5, 10, 20):
+                    header = ['N%02d' % num for num in range(size)]
+                    literal = ','.join(header) + '\n' + self.generate_matrix_csv(size, symmetric=symmetric)
+                    graph = dengraph.graphs.graph_io.csv_graph_reader(
+                        literal.splitlines(), nodes_header=True, symmetric=symmetric
+                    )
+                    self.assertHeaderMatrixGraph(header, graph)
 
     def test_header_call(self):
         """Test header from first line as strings"""
-        for size in (1, 5, 10, 20):
-            header = ['%2d' % num for num in range(size)]
-            literal = ','.join(header) + '\n' + self.generate_matrix_csv(size)
-            graph = dengraph.graphs.graph_io.csv_graph_reader(literal.splitlines(), nodes_header=lambda elem: int(elem))
-            self.assertHeaderMatrixGraph(list(range(size)), graph)
+        for symmetric in (True, False):
+            with self.subTest(symmetric=symmetric):
+                for size in (1, 5, 10, 20):
+                    header = ['%2d' % num for num in range(size)]
+                    literal = ','.join(header) + '\n' + self.generate_matrix_csv(size, symmetric=symmetric)
+                    graph = dengraph.graphs.graph_io.csv_graph_reader(
+                        literal.splitlines(), nodes_header=lambda elem: int(elem), symmetric=symmetric
+                    )
+                    self.assertHeaderMatrixGraph(list(range(size)), graph)
 
     def assertHeaderMatrixGraph(self, header, graph):
             self.assertEqual(sorted(graph), sorted(header))
@@ -94,4 +114,4 @@ class GraphIOTest(unittest.TestCase):
                         with self.assertRaises(dengraph.graph.NoSuchEdge):
                             graph[node_from:node_to]
                     else:
-                        self.assertEqual(graph[node_from:node_to], row_idx - column_idx)
+                        self.assertEqual(graph[node_from:node_to], abs(column_idx - row_idx))
