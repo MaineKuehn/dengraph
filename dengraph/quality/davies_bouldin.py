5831d58fdf060caab425651fb5c65cb9950a292c
Eileen Kuehn <eileen.kuehn@kit.edu>
2016-11-12 12:05:00 +0100

    raising value error when no sufficient amount of clusters is provided for davies bouldin

--- dengraph/quality/davies_bouldin.py
+++ dengraph/quality/davies_bouldin.py
@@ -15,33 +15,35 @@ def davies_bouldin_score(clusters, graph):
     :param graph: The underlying graph that offers distance function
     :return: Calculated Davies-Bouldin score
     """
-    score_cache = {}
-    means_cache = {}
-    result = 0
-    for cluster_1 in clusters:
-        maximum_distance = 0
-        for cluster_2 in clusters:
-            if cluster_1 != cluster_2:
-                try:
-                    means_cache[cluster_1]
-                except KeyError:
-                    means_cache[cluster_1] = graph.distance.mean(list(cluster_1))
-                    score_cache[cluster_1] = inter_cluster_mean_score(
-                        cluster_1,
-                        graph,
-                        means_cache[cluster_1])
-                try:
-                    means_cache[cluster_2]
-                except KeyError:
-                    means_cache[cluster_2] = graph.distance.mean(list(cluster_2))
-                    score_cache[cluster_2] = inter_cluster_mean_score(
-                        cluster_2,
-                        graph,
-                        means_cache[cluster_2])
+    if len(clusters) > 1:
+        score_cache = {}
+        means_cache = {}
+        result = 0
+        for cluster_1 in clusters:
+            maximum_distance = 0
+            for cluster_2 in clusters:
+                if cluster_1 != cluster_2:
+                    try:
+                        means_cache[cluster_1]
+                    except KeyError:
+                        means_cache[cluster_1] = graph.distance.mean(list(cluster_1))
+                        score_cache[cluster_1] = inter_cluster_mean_score(
+                            cluster_1,
+                            graph,
+                            means_cache[cluster_1])
+                    try:
+                        means_cache[cluster_2]
+                    except KeyError:
+                        means_cache[cluster_2] = graph.distance.mean(list(cluster_2))
+                        score_cache[cluster_2] = inter_cluster_mean_score(
+                            cluster_2,
+                            graph,
+                            means_cache[cluster_2])
 
-                distance = (score_cache[cluster_1] + score_cache[cluster_2]) / \
-                    graph.distance(means_cache[cluster_1], means_cache[cluster_2])
-                if distance > maximum_distance:
-                    maximum_distance = distance
-        result += maximum_distance
-    return result / float(len(list(clusters)))
+                    distance = (score_cache[cluster_1] + score_cache[cluster_2]) / \
+                        graph.distance(means_cache[cluster_1], means_cache[cluster_2])
+                    if distance > maximum_distance:
+                        maximum_distance = distance
+            result += maximum_distance
+        return result / float(len(list(clusters)))
+    raise ValueError
